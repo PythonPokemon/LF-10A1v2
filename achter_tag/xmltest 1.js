e31871ad00d2089f16af6cb61448f030f54161ea
PythonPokemon <Jakob.D.Baron@Outlook.de>
2024-04-04 10:51:28 +0200

    update

--- achter_tag/xmltest 1.js
+++ achter_tag/xmltest 1.js
@@ -9,9 +9,25 @@ function meinAnfragenFunktion(){
         console.log(meineXML);
         console.log(meineUnparsedXML);
 
-        document.getElementById('output').value = meineXML.getElementsByTagName('beschreibung')[1].textContent;
+        // Hier passen wir den Code an, um auf die "class.xml" zuzugreifen und den gewünschten Inhalt abzurufen
+        const klasseLF10a = meineXML.getElementsByTagName('klasse')[0];
+        const personen = klasseLF10a.getElementsByTagName('person');
+        const firstName = personen[0].getElementsByTagName('firstname')[0].textContent;
+        const lastName = personen[0].getElementsByTagName('lastname')[0].textContent;
+        const city = personen[0].getElementsByTagName('city')[0].textContent;
+        const country = personen[0].getElementsByTagName('country')[0].textContent;
+        const codename = personen[0].getElementsByTagName('codename')[0].textContent;
+        const email = personen[0].getElementsByTagName('email')[0].textContent;
+
+        // Beispielhaft zeigen wir den ersten Eintrag der XML in der Konsole
+        console.log("First Name:", firstName);
+        console.log("Last Name:", lastName);
+        console.log("City:", city);
+        console.log("Country:", country);
+        console.log("Codename:", codename);
+        console.log("Email:", email);
     }
-    meineAnfragenMaschine.open("GET", "data.xml");
+    meineAnfragenMaschine.open("GET", "class.xml"); // Ändern Sie den Pfad zu class.xml
     meineAnfragenMaschine.send();
 }
 
